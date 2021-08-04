214da56446244565e203cbb1ebad408de8a15339
chaosh-liang <chao.liang.fly@gmail.com>
2021-08-04 21:36:19 +0800

    feat: 类别下的系列，系列下的商品列表

--- src/routes/goods.js
+++ src/routes/goods.js
@@ -21,6 +21,21 @@ router.get('/home/products', async ctx => {
   ctx.body = { code: 200, data: res, error_msg: '' };
 });
 
+// 某类别某系列下的商品
+router.get('/series/:id', async ctx => {
+  const { request: { params: { id } } } = ctx;
+  const res = await Goods.find({ series_id: ObjectId(id) }).exec();
+  const lite = res.map(item => ({
+    _id: item._id,
+    icon_url: item.icon_url,
+    name: item.name,
+    price: item.price,
+    desc: item.desc,
+    currency_unit: item.currency_unit
+  }));
+  ctx.body = { code: 200, data: lite, error_msg: '' };
+});
+
 // 商品详情
 router.get('/detail/:id', async ctx => {
   const { request: { params: { id } } } = ctx;
