e75bb2185ec7a3028681dbd4f2de1dc647b69cb7
chaosh-liang <chao.liang.fly@gmail.com>
2021-08-17 01:29:37 +0800

    fix: something format

--- src/model/categories.js
+++ src/model/categories.js
@@ -4,6 +4,9 @@ const { Schema, model } = mongoose;
 const schema = new Schema(
   {
     name: String,
+    desc: String,
+    order: Number, // 序号
+    icon_url: String
   },
   { timestamps: { createdAt: 'created_time', updatedAt: 'update_time' } }
 );
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -5,29 +5,36 @@ const { ObjectId } = require('mongodb');
 
 // 左侧菜单
 router.get('/', async ctx => {
-  const res = await Category.find(); // 所有菜单
-  ctx.body = { error_code: '00', data: res, error_msg: 'Success' };
+  const res = await Category.aggregate()
+    .lookup({
+      from: 'series',
+      localField: '_id',
+      foreignField: 'category_id',
+      as: 'series_data',
+    })
+    .sort({ order: 1 })
+  ctx.body = { error_code: '00', data: { res }, error_msg: 'Success' };
 });
 
 // 某个类别的详情
 router.get('/detail/:id', async ctx => {
   const { request: { params: { id } } } = ctx;
   const res = await Category.find({ _id: ObjectId(id) });
-  ctx.body = { error_code: '00', data: res, error_msg: 'Success' };
+  ctx.body = { error_code: '00', data: { res }, error_msg: 'Success' };
 });
 
 // 某个类别下的所有系列
 router.get('/s/:id', async ctx => {
   const { request: { params: { id } } } = ctx;
   const res = await Series.find({ category_id: ObjectId(id) });
-  ctx.body = { error_code: '00', data: res, error_msg: 'Success' };
+  ctx.body = { error_code: '00', data: { res }, error_msg: 'Success' };
 });
 
 // 某个系列的详情
 router.get('/s/detail/:id', async ctx => {
   const { request: { params: { id } } } = ctx;
   const res = await Series.find({ _id: ObjectId(id) });
-  ctx.body = { error_code: '00', data: res, error_msg: 'Success' };
+  ctx.body = { error_code: '00', data: { res }, error_msg: 'Success' };
 });
 
 module.exports = router.routes();
